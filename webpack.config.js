1a8a225d6e98f51799e749003f4822ae38530b97
William Eakin <w.eakin@gmail.com>
2016-09-14 12:50:35 -0700

    added uglifyjs to plugins, and devtool: source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 var webpack = require('webpack');
 
 module.exports = {
+    devtool: "source-map",
     entry: [
         'webpack-dev-server/client?http://localhost:8080',
         'webpack/hot/only-dev-server',
         './src/index.js'
-], module: {
+],
+module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
         }]
-}, resolve: {
+},
+resolve: {
         extensions: ['', '.js', '.jsx']
     },
     output: {
         path: 'dist/assets/js',
-        publicPath: '/',
+        publicPath: '/assets/js',
         filename: 'base.js'
     },
     devServer: {
         contentBase: './dist',
         hot: true
-}, plugins: [
-        new webpack.HotModuleReplacementPlugin()
+},
+plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false
+        },
+      })
     ]
 };
