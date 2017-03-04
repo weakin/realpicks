c538e2104bd0f12b89948bf6b735bdb146202d85
William Eakin <w.eakin@gmail.com>
2017-03-04 10:26:51 -0800

    changed test framework from tape to mocha

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-    devtool: "source-map",
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src/index.js'
-],
-module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-},
-resolve: {
-        extensions: ['', '.js', '.jsx']
+  devtool: "source-map",
+  entry: [
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+        extensions: ['.js', '.jsx']
     },
     output: {
         path: 'dist/assets/js',
         publicPath: '/assets/js',
         filename: 'base.js'
     },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-},
+  devServer: {
+    contentBase: './dist',
+    port: 80,
+    historyApiFallback: true,
+    hot: true,
+    public: 'localhost:80'
+  },
 plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false
-        },
-      })
-    ]
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+]
 };
