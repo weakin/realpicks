e8ee3ae7d280c14fac68c5ca54e67fa57d08c566
William Eakin <w.eakin@gmail.com>
2017-03-04 11:58:42 -0800

    changed local dev database from realpicks to realpicks2

--- src/assets/php/credentials.php
+++ src/assets/php/credentials.php
@@ -1,7 +1,7 @@
 <?php
 
 function getDatabaseCredentials() {
-    return "host=localhost dbname=realpicks user=webuser password=web"; // <- comment out for production
+    return "host=localhost dbname=realpicks2 user=webuser password=web"; // <- comment out for production
     return "host=ec2-54-221-246-85.compute-1.amazonaws.com dbname=denln9aq3m9rvk port=5432 user=xhrrdkwsadhkgf password=at9j3hAj43Q-OH1YML3u-G2Acm";
 }
 
