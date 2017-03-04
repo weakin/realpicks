2453b059514254650ce2aabcf740526a9c2e8ce7
William Eakin <w.eakin@gmail.com>
2017-03-04 11:30:35 -0800

    added query to pull and display game_week_alias from the nfl_weeks table

--- src/assets/php/show_user_stats.php
+++ src/assets/php/show_user_stats.php
@@ -3,22 +3,30 @@ header('Content-Type: application/json');
 
 include 'helper_functions.php';
 include 'credentials.php';
+include 'current_week.php';
 
 // the string and number validation functions are defined in helper_functions.php
 
 $week = validateFormNumberInput($_GET['week']);
 $userGroup = validateFormStringInput($_GET['group']);
+$requestedWeek = getWeek($_GET['week']);
+
+if ($week == 0) {
+    $week = $requestedWeek[1];
+}
 
 $winningPicks = 'week_' . $week . '_wn';
 $losingPicks = 'week_' . $week . '_ls';
 
 $dbConn = pg_connect(getDatabaseCredentials()) or die('Could not connect: ' . pg_last_error());
 
-$weeklyPicksStatsQuery = "SELECT user_name, $winningPicks, $losingPicks FROM user_stats WHERE user_group = '$userGroup' ORDER BY coalesce($winningPicks,0) DESC";
-$totalPicksStatsQuery = "SELECT user_name, total_wn, total_ls FROM user_stats WHERE user_group = '$userGroup' ORDER BY coalesce(total_wn,0) DESC, user_name ASC";
+$weeklyPicksStatsQuery = "SELECT user_name, $winningPicks, $losingPicks FROM user_stats WHERE user_group = '$userGroup' ORDER BY coalesce($winningPicks,0) DESC, user_name ASC";
+$totalPicksStatsQuery = "SELECT user_name, total_wn, total_ls FROM user_stats WHERE user_group = '$userGroup' ORDER BY coalesce(total_wn,0) DESC";
+$weeklyAliasQuery = "SELECT game_week_alias FROM nfl_weeks WHERE game_week = $week";
 
 $weeklyPicks = pg_query($weeklyPicksStatsQuery) or die('weeklyPicksStatsQuery failed: ' . pg_last_error());
 $totalPicks = pg_query($totalPicksStatsQuery) or die('totalPicksStatsQuery failed: ' . pg_last_error());
+$gameWeekAlias = pg_query($weeklyAliasQuery) or die('weeklyAliasQuery failed: ' . pg_last_error());
 
 pg_close($dbConn);
 
@@ -32,6 +40,7 @@ if ($weeklyPicksRowsAffected == 0 && $totalPicksRowsAffected == 0) {
 
 $weekPicksArray = pg_fetch_all($weeklyPicks);
 $totalPicksArray = pg_fetch_all($totalPicks);
+$gameWeekAliasArray = pg_fetch_all($gameWeekAlias);
 
 $weeklyStandings = [];
 $totalStandings = [];
@@ -44,7 +53,7 @@ foreach($weekPicksArray as $key => $value) {
     if ($value[$losingPicks] == null) {
         $value[$losingPicks] = 0;
     }
-    $weeklyStandings[$value['user_name']] = $value[$winningPicks] . ' - ' . $value[$losingPicks];
+    $weeklyStandings[trim($value['user_name'])] = $value[$winningPicks] . ' - ' . $value[$losingPicks];
 }
 
 foreach($totalPicksArray as $key2 => $value2) {
@@ -54,11 +63,14 @@ foreach($totalPicksArray as $key2 => $value2) {
     if ($value2['total_ls'] == null) {
         $value2['total_ls'] = 0;
     }
-    $totalStandings[$value2['user_name']] = $value2['total_wn'] . ' - ' . $value2['total_ls'];
+    $totalStandings[trim($value2['user_name'])] = $value2['total_wn'] . ' - ' . $value2['total_ls'];
 }
 
-$allStandings['week ' . $week] = $weeklyStandings;
+$allStandings['week'] = $weeklyStandings;
 $allStandings['total'] = $totalStandings;
+$allStandings['current_week'] = $requestedWeek[1];
+$allStandings['requested_week'] = $requestedWeek[0];
+$allStandings['game_week_alias'] = trim($gameWeekAliasArray[0]['game_week_alias']);
 
 echo json_encode($allStandings);
 
