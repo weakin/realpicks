9bbe609110ade9645a484550d7f61f0bd433e67e
William Eakin <w.eakin@gmail.com>
2017-03-06 12:58:02 -0800

    added sourceMap: true to the UglifyJs config

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
 plugins: [
   new webpack.HotModuleReplacementPlugin(),
   new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
     compress: {
       warnings: false
     },
