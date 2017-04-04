0287fdf743d6b4dd7f9c0aac9d175b2835fe197d
William Eakin <w.eakin@gmail.com>
2017-04-04 09:26:48 -0700

    scrollToTop was preventing page usability
    When the component initialy loaded, or updated, the scrollToTop function was called. The problem is that
    if the page was already at the top the function prevented the user from scrolling down the page even if
    the data was loaded. The function was changed to check to see if the page has been scrolled, and if so
    by how much. Depending on the ammount the page has been scrolled, it adjusts the time accordingly.

--- src/pages/Picks.jsx
+++ src/pages/Picks.jsx
@@ -89,7 +89,14 @@ class Picks extends React.Component {
   }
 
   scrollToTop () {
-    $('html,body').animate({scrollTop: 0}, 1000)
+    let top = window.scrollY
+    if (top > 100 && top < 499) {
+      $('html,body').animate({scrollTop: 0}, 250)
+    } else if (top > 500 && top < 999) {
+      $('html,body').animate({scrollTop: 0}, 500)
+    } else if (top > 1000) {
+      $('html,body').animate({scrollTop: 0}, 750)
+    }
   }
 
   renderGamePicks (Obj) {
