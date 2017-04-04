cb30bc4c904241be768cb34ed0753a90b439984f
William Eakin <w.eakin@gmail.com>
2017-04-03 17:21:37 -0700

    removed navClickHandler as it was no longer needed. Swapped out addWindowListerer for hashChangeListener. Added add and remove event listeners for when the component will mount and unmount

--- src/pages/Picks.jsx
+++ src/pages/Picks.jsx
@@ -6,38 +6,53 @@ class Picks extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = {group: this.props.group, week: '', gameWeekAlias: '', users: [], weeksGames: [], picks: {}, currentWeek: 0, requestedWeek: 0, viewedPicks: [], viewedPicksObjects: []}
+    this.state = {group: this.props.group, week: '', gameWeekAlias: '', users: [], weeksGames: [], picks: {}, current_week: 0, requestedWeek: 0, viewedPicks: [], viewedPicksObjects: []}
 
     this.fetchPicksInfo = this.fetchPicksInfo.bind(this)
     this.renderGamePicks = this.renderGamePicks.bind(this)
-    this.navClickHandler = this.navClickHandler.bind(this)
-    this.addWindowListener = this.addWindowListener.bind(this)
+    this.hashChangeListener = this.hashChangeListener.bind(this)
     this.scrollToTop = this.scrollToTop.bind(this)
   }
 
   componentDidMount () {
     this.fetchPicksInfo(this.props.params.week)
-    this.addWindowListener()
+    window.addEventListener('hashchange', this.hashChangeListener, false)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('hashchange', this.hashChangeListener, false)
   }
 
   componentDidUpdate () {
     this.scrollToTop()
   }
 
-  addWindowListener () {
-    let viewedPicks = this.state.viewedPicks
-    let viewedPicksObjects = this.state.viewedPicksObjects
-    let thisHack = this
-    window.addEventListener('popstate', function (e) {
-      let pickRegEx = /picks\/(\d{1,2})/
-      let foundPickWeek = document.URL.match(pickRegEx)
-      let foundPickWeekInt = foundPickWeek !== null ? Number(foundPickWeek[1]) : null
-      if (viewedPicks.indexOf(foundPickWeekInt) > -1) {
-        thisHack.setState(viewedPicksObjects[foundPickWeekInt])
-      } else if (foundPickWeekInt === null) {
-        thisHack.setState(viewedPicksObjects[viewedPicks[0]])
-      }
-    })
+  hashChangeListener () {
+    console.log('Picks.jsx - addEventListener')
+    let pickRegEx = /picks\/(\d{1,2})/
+    let foundPickWeek = document.URL.match(pickRegEx)
+    let foundPickWeekInt = foundPickWeek !== null ? Number(foundPickWeek[1]) : null
+    if (this.state.viewedPicks.indexOf(foundPickWeekInt) === -1 && foundPickWeekInt !== null && this.state.current_week !== (foundPickWeekInt + 1)) {
+      // the requested data has not been viewed, there is an integer from location.hash, and that int is not one less than the current week int
+      console.log('addEventListener - if block: 1')
+      this.fetchPicksInfo(foundPickWeekInt)
+    } else if (this.state.viewedPicks.indexOf(foundPickWeekInt) === -1 && foundPickWeekInt !== null && (this.state.current_week - 1) === foundPickWeekInt) {
+      // the requested data has not been viewed, there is an integer from location.hash, and that int is one less than the current week int
+      console.log('addEventListener - if block: 2')
+      this.fetchPicksInfo(foundPickWeekInt)
+    } else if (this.state.viewedPicks.indexOf(this.state.current_week) === -1 && foundPickWeekInt === null && this.state.viewedPicks.length > 0) {
+      // the requested data has not been viewed, there is no integer from location.hash, and as there are no other weeks in memory, display the current week
+      console.log('addEventListener - if block: 3')
+      this.fetchPicksInfo(this.state.current_week)
+    } else if (this.state.viewedPicks.indexOf(this.state.current_week) > -1 && foundPickWeekInt === null) {
+      // the requested data has already been viewed, is the current week, and there is no week integer coming in from location.hash, so update the state from the stored data
+      console.log('addEventListener - if block: 4')
+      this.setState(this.state.viewedPicksObjects[this.state.current_week])
+    } else if (this.state.viewedPicks.indexOf(foundPickWeekInt) > -1) {
+      console.log('addEventListener - if block: 5')
+      // the requested data has already been viewed and is stored, so update the state from the stored data
+      this.setState(this.state.viewedPicksObjects[foundPickWeekInt])
+    }
   }
 
   fetchPicksInfo (weekInt) {
@@ -54,7 +69,7 @@ class Picks extends React.Component {
             this.state.viewedPicks.push(pickWeekInt)
           }
           if (response.body.week_and_users === undefined) {
-            let emptyObject = {picks: null, week: null, users: null, weeksGames: null, currentWeek: response.body.current_week, requestedWeek: response.body.requested_week, gameWeekAlias: response.body.game_week_alias}
+            let emptyObject = {picks: null, week: null, users: null, weeksGames: null, current_week: response.body.current_week, requestedWeek: response.body.requested_week, gameWeekAlias: response.body.game_week_alias}
             this.state.viewedPicksObjects[pickWeekInt] = emptyObject
             this.setState(emptyObject)
             return
@@ -63,7 +78,7 @@ class Picks extends React.Component {
           let week = response.body.week_and_users.shift()
           let users = response.body.week_and_users.sort()
           let weeksGames = response.body[week]
-          let pickObject = {picks: response.body.picks, week: week, users: users, weeksGames: weeksGames, currentWeek: response.body.current_week, requestedWeek: response.body.requested_week, gameWeekAlias: response.body.game_week_alias}
+          let pickObject = {picks: response.body.picks, week: week, users: users, weeksGames: weeksGames, current_week: response.body.current_week, requestedWeek: response.body.requested_week, gameWeekAlias: response.body.game_week_alias}
           this.state.viewedPicksObjects[pickWeekInt] = pickObject
           this.setState(pickObject)
         } else {
@@ -75,10 +90,6 @@ class Picks extends React.Component {
 
   scrollToTop () {
     $('html,body').animate({scrollTop: 0}, 1000)
-  }
-
-  navClickHandler (weekInt) {
-    this.fetchPicksInfo(weekInt)
   }
 
   renderGamePicks (Obj) {
@@ -98,7 +109,7 @@ class Picks extends React.Component {
     let firstKey = 0
     let keys = []
     let allPicks = ''
-    let headerText = this.state.gameWeekAlias 
+    let headerText = this.state.gameWeekAlias
     if (this.state.picks !== undefined && this.state.picks !== null) {
       keysArr = Object.keys(this.state.picks)
       firstKey = keysArr[0]
@@ -128,7 +139,7 @@ class Picks extends React.Component {
     }
 
     return <div className='picks'>
-      <Pick caller='header' headerArray={keys} requestedWeek={this.state.requestedWeek} currentWeek={this.state.currentWeek} gameWeekAlias={headerText} navClickHandler={this.navClickHandler} />
+      <Pick caller='header' headerArray={keys} requestedWeek={this.state.requestedWeek} currentWeek={this.state.current_week} gameWeekAlias={headerText} />
       {allPicks}
     </div>
   }
